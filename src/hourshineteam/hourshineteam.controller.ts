3110c1db69ca9b2039c88412e54860439d61d5d0
Techinfomatic <53048119+Developmenteam30@users.noreply.github.com>
2022-09-13 12:03:51 +0530

    update

--- src/hourshineteam/hourshineteam.controller.ts
+++ src/hourshineteam/hourshineteam.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { HourshineteamService } from './hourshineteam.service';
 import { CreateHourshineteamDto } from './dto/create-hourshineteam.dto';
 import { UpdateHourshineteamDto } from './dto/update-hourshineteam.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/users/jwt-auth.guard';
 
 @Controller('hourshineteam')
 export class HourshineteamController {
   constructor(private readonly hourshineteamService: HourshineteamService) {}
 
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createHourshineteamDto: CreateHourshineteamDto) {
     return this.hourshineteamService.create(createHourshineteamDto);
@@ -17,16 +21,22 @@ export class HourshineteamController {
     return this.hourshineteamService.findAll();
   }
 
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.hourshineteamService.findOne(+id);
   }
 
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateHourshineteamDto: UpdateHourshineteamDto) {
     return this.hourshineteamService.update(+id, updateHourshineteamDto);
   }
 
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.hourshineteamService.remove(+id);
